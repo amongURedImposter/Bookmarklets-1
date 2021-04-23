ff6cf59b746a0255721763fcdfab26b0bd41cca0
LQR471814 <42160088+LQR471814@users.noreply.github.com>
2021-04-22 22:03:14 -0700

    Added some nice things

--- SchoologyRound/SchoologyRound.ts
+++ SchoologyRound/SchoologyRound.ts
@@ -1,48 +1,63 @@
 function getFirstXPATHElement(xpath: string, context?: Element): HTMLElement | null {
-    return document.evaluate(
-        xpath,
-        context ? context : document,
-        null,
-        XPathResult.FIRST_ORDERED_NODE_TYPE,
-        null
-    ).singleNodeValue as HTMLElement
+  return document.evaluate(
+    xpath,
+    context ? context : document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue as HTMLElement
 }
 
 function moveChildren(children: HTMLCollection, newParent: HTMLElement, callback: (child: HTMLElement) => void) {
-    const moveChildren = []
+  const moveChildren = []
 
-    for (const child of children) {
-        console.log(child)
-        callback(child as HTMLElement)
-        moveChildren.push(child)
-    }
+  for (const child of children) {
+    console.log(child)
+    callback(child as HTMLElement)
+    moveChildren.push(child)
+  }
 
-    for (const child of moveChildren) {
-        newParent.prepend(child)
-    }
+  for (const child of moveChildren) {
+    newParent.prepend(child)
+  }
 }
+
+// //? Will make element maintain style (className) when it is removed and re-added (aka. re-render)
+// function attachStyleMaintainer(elementParent: HTMLElement, style: string) {
+//   let reAppend: Node | null = null
+
+//   const observer = new MutationObserver(
+//     (mutationRecords) => {
+//       for (const record of mutationRecords) {
+//         for (const node of record.removedNodes) {
+//           if ((node as HTMLElement).className === style) {
+//             reAppend = node
+//             return
+//           }
+//         }
+
+//         for (const node of record.addedNodes) {
+
+//         }
+//       }
+//     }
+//   )
+
+//   observer.observe(elementParent, {
+//     attributes: true,
+//     childList: true,
+//     subtree: true
+//   })
+// }
 
 //* Make Navbar sticky and nicer
 const newParent = document.getElementById('body')!
 const headerChildren = getFirstXPATHElement(`//header`)!.children
 
 moveChildren(headerChildren, newParent, (child) => {
-    if (child.tagName !== "NAV" || !child) {
-        return
-    }
+  if (child.tagName !== "NAV" || !child) return
 
-    child.classList.add('mod-style-stickyNav')
-
-    for (const navBarParent of child.children) {
-        for (const navBarChild of navBarParent.children) {
-            (navBarChild as HTMLElement).style.marginLeft = "10px";
-
-            const childButton = (getFirstXPATHElement(`.//*[name()='button' or name()='a']`, navBarChild) as HTMLElement)
-            if (childButton) {
-                childButton.classList.add('mod-style-navMenuButton')
-            }
-        }
-    }
+  child.classList.add('mod-style-stickyNav')
 })
 
 getFirstXPATHElement(`//div[@id='header']`)!.remove()
